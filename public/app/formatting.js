5202a332dd482cc363759b82c189a49c1e23a49d
Theo <theo@iconara.net>
2012-08-03 15:03:36 +0200

    Moved literal "n/a" to a filter

--- public/app/formatting.js
+++ public/app/formatting.js
@@ -18,17 +18,28 @@
     [Infinity,            "in the distant past",    0               ,  "ceil"]
   ]
 
-  angular.module("ec2pricing.formatting", [])
-    .filter("timeAgo", function () {
-      return function (input, reference) {
-        if (input == null) return ""
-        reference = reference || new Date()
-        var diff = (reference.getTime() - input.getTime())/1000
-        var e = _(TIMES_AGO).find(function (e) { return diff < e[0] })
-        var text = e[1]
-        var period = e[2]
-        var rounding = e[3]
-        return _.str.sprintf(text, Math[rounding](diff/period))
+  var module = angular.module("ec2pricing.formatting", [])
+
+  module.filter("timeAgo", function () {
+    return function (input, reference) {
+      if (input == null) return ""
+      reference = reference || new Date()
+      var diff = (reference.getTime() - input.getTime())/1000
+      var e = _(TIMES_AGO).find(function (e) { return diff < e[0] })
+      var text = e[1]
+      var period = e[2]
+      var rounding = e[3]
+      return _.str.sprintf(text, Math[rounding](diff/period))
+    }
+  })
+
+  module.filter("naIfNull", function () {
+    return function (input) {
+      if (!input) {
+        return "n/a"
+      } else {
+        return input
       }
-    })
+    }
+  })
 })()
