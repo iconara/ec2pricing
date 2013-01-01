7b8b8cca60203afba51d33985717ccf62a2cb404
Theo <theo@iconara.net>
2013-01-01 18:36:17 +0100

    Implemented TTL in HeapCache

--- lib/ec2_pricing/heap_cache.rb
+++ lib/ec2_pricing/heap_cache.rb
@@ -2,16 +2,26 @@
 
 module Ec2Pricing
   class HeapCache
-    def initialize
+    def initialize(options={})
+      @ttl = options[:ttl]
+      @time = options[:time] || Time
       $heap_cache_storage ||= {}
+      $heap_cache_expires ||= {}
     end
 
     def []=(key, value)
+      $heap_cache_expires[key] = @time.now.to_i + @ttl if @ttl
       $heap_cache_storage[key] = value
     end
 
     def [](key)
-      $heap_cache_storage[key]
+      expires = $heap_cache_expires[key]
+      if expires.nil? || @time.now.to_i < expires
+        $heap_cache_storage[key]
+      else
+        $heap_cache_storage.delete(key)
+        nil
+      end
     end
   end
 end
--- spec/ec2_pricing/heap_cache_spec.rb
+++ spec/ec2_pricing/heap_cache_spec.rb
@@ -6,7 +6,15 @@ require 'spec_helper'
 module Ec2Pricing
   describe HeapCache do
     let :cache do
-      described_class.new
+      described_class.new(ttl: 5, time: fake_time)
+    end
+
+    let :fake_time do
+      stub(:time)
+    end
+
+    before do
+      fake_time.stub(:now).and_return(1)
     end
 
     it 'stores stuff' do
@@ -15,9 +23,15 @@ module Ec2Pricing
     end
 
     it 'makes everything available from any instance' do
-      another_cache = described_class.new
+      another_cache = described_class.new(time: fake_time)
       cache['stuff'] = 'lots of it'
       expect(another_cache['stuff']).to eql('lots of it')
+    end
+
+    it 'expires stuff' do
+      cache['something'] = 3
+      fake_time.stub(:now).and_return(6)
+      expect(cache['something']).to be_nil
     end
   end
 end
