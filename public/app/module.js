d276234bd3ea123b06b65c2b0bd4fd42c39c2ab2
Theo <theo@iconara.net>
2013-01-07 17:30:23 +0100

    Now reading data locally instead of from the API

--- public/app/module.js
+++ public/app/module.js
@@ -1,3 +1,4 @@
 angular.module("ec2Pricing", ["ec2pricing.sorting", "ec2pricing.tracking", "ec2pricing.formatting", "ec2pricing.pricing"])
-  .value("instanceTypesByRegionUrl", "/api/v1/")
+  .value("instanceTypesByRegionUrl", "data/data.json")
+  // .value("instanceTypesByRegionUrl", "/api/v1/")
   .value("spotPricingUrl", "http://spot-price.s3.amazonaws.com/spot.js")
