c2a13580551976efa188f5ea1e27522bb58ebadd
Theo <theo@iconara.net>
2013-11-15 14:59:23 +0100

    Fix tests for the pricing parser

--- spec/ec2_pricing/pricing_parser_spec.rb
+++ spec/ec2_pricing/pricing_parser_spec.rb
@@ -11,16 +11,19 @@ module Ec2Pricing
 
     let :pricing_data do
       {
-        :on_demand => MultiJson.load(File.read(File.expand_path('../../resources/pricing-on-demand-instances.json', __FILE__))),
+        :linux => MultiJson.load(File.read(File.expand_path('../../resources/linux-od.json', __FILE__))),
+        :mswin => MultiJson.load(File.read(File.expand_path('../../resources/mswin-od.json', __FILE__))),
+        :rhel => MultiJson.load(File.read(File.expand_path('../../resources/rhel-od.json', __FILE__))),
+        :sles => MultiJson.load(File.read(File.expand_path('../../resources/sles-od.json', __FILE__))),
         :emr => MultiJson.load(File.read(File.expand_path('../../resources/pricing-emr.json', __FILE__))),
         :spot => MultiJson.load(File.read(File.expand_path('../../resources/spot.js', __FILE__)).sub(/\A\s*callback\((.+)\)\s*\Z/m, '\1').gsub(/\},\s*\]/, '}]')),
       }
     end
 
     describe '#parse' do
-      [:on_demand, :emr, :spot].each do |source|
-        source_name = {:on_demand => 'on demand', :emr => 'EMR', :spot => 'spot'}[source]
-        instance_type_count = {:on_demand => 17, :emr => 11, :spot => 17}[source]
+      [:linux, :mswin, :rhel, :sles, :emr, :spot].each do |source|
+        source_name = {:linux => 'on demand (Linux)', :mswin => 'on demand (Windows)', :rhel => 'on demand (Red Hat)', :sles => 'on demand (SLES)', :emr => 'EMR', :spot => 'spot'}[source]
+        instance_type_count = {:linux => 23, :mswin => 23, :rhel => 23, :sles => 23, :emr => 11, :spot => 22}[source]
 
         context "with #{source_name} pricing data" do
           let :pricing do
@@ -43,27 +46,36 @@ module Ec2Pricing
             expect(instance_type).not_to be_nil
           end
 
-          it 'handles the cc1/cc2 special case' do
-            region = pricing.find { |region| region[:region] == 'us-east-1' }
-            cc1_4xlarge = region[:instance_types].find { |instance_type| instance_type[:api_name] == 'cc1.4xlarge' }
-            cc2_8xlarge = region[:instance_types].find { |instance_type| instance_type[:api_name] == 'cc2.8xlarge' }
-            expect(cc1_4xlarge).not_to be_nil
-            expect(cc2_8xlarge).not_to be_nil
+          if source == :emr
+            it 'finds the EMR pricing' do
+              region = pricing.find { |region| region[:region] == 'sa-east-1' }
+              instance_type = region[:instance_types].find { |instance_type| instance_type[:api_name] == 'm2.xlarge' }
+              expect(instance_type[:pricing][:linux]).to be_a(Numeric)
+            end
           end
 
-          case source
-          when :on_demand, :spot
+          if source == :spot
+            it 'handles the cc1/cc2 special case' do
+              region = pricing.find { |region| region[:region] == 'us-east-1' }
+              cc1_4xlarge = region[:instance_types].find { |instance_type| instance_type[:api_name] == 'cc1.4xlarge' }
+              cc2_8xlarge = region[:instance_types].find { |instance_type| instance_type[:api_name] == 'cc2.8xlarge' }
+              expect(cc1_4xlarge).not_to be_nil
+              expect(cc2_8xlarge).not_to be_nil
+            end
+
             it 'finds the pricing for Linux and Windows' do
               region = pricing.find { |region| region[:region] == 'sa-east-1' }
               instance_type = region[:instance_types].find { |instance_type| instance_type[:api_name] == 'm2.xlarge' }
               expect(instance_type[:pricing][:linux]).to be_a(Numeric)
               expect(instance_type[:pricing][:mswin]).to be_a(Numeric)
             end
-          else
-            it 'finds the EMR pricing' do
+          end
+
+          if source != :emr && source != :spot
+            it 'finds the pricing for the OS' do
               region = pricing.find { |region| region[:region] == 'sa-east-1' }
               instance_type = region[:instance_types].find { |instance_type| instance_type[:api_name] == 'm2.xlarge' }
-              expect(instance_type[:pricing][:linux]).to be_a(Numeric)
+              expect(instance_type[:pricing][source]).to be_a(Numeric)
             end
           end
         end
