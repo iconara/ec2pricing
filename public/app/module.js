58b01b17a89428e5d4ffbf6817d09afd84f83afa
Theo <theo@iconara.net>
2014-04-20 11:58:37 +0200

    Detect pricing categories directly from the URLs

--- public/app/module.js
+++ public/app/module.js
@@ -25,76 +25,77 @@
     }
   })
 
-  ec2pricing.value("pricingUrls", {
-    "onDemand": [
-      "http://a0.awsstatic.com/pricing/1/ec2/linux-od.min.js",
-      "http://a0.awsstatic.com/pricing/1/ec2/previous-generation/linux-od.min.js",
-      "http://a0.awsstatic.com/pricing/1/ec2/mswin-od.min.js",
-      "http://a0.awsstatic.com/pricing/1/ec2/previous-generation/mswin-od.min.js",
-      "http://a0.awsstatic.com/pricing/1/ec2/rhel-od.min.js",
-      "http://a0.awsstatic.com/pricing/1/ec2/previous-generation/rhel-od.min.js",
-      "http://a0.awsstatic.com/pricing/1/ec2/sles-od.min.js",
-      "http://a0.awsstatic.com/pricing/1/ec2/previous-generation/sles-od.min.js",
-      "http://a0.awsstatic.com/pricing/1/ec2/mswinSQL-od.min.js",
-      "http://a0.awsstatic.com/pricing/1/ec2/previous-generation/mswinSQL-od.min.js",
-      "http://a0.awsstatic.com/pricing/1/ec2/mswinSQLWeb-od.min.js",
-      "http://a0.awsstatic.com/pricing/1/ec2/previous-generation/mswinSQLWeb-od.min.js"
-    ],
-    "lightReservation": [
-      "http://a0.awsstatic.com/pricing/1/ec2/linux-ri-light.min.js",
-      "http://a0.awsstatic.com/pricing/1/ec2/previous-generation/light_linux.min.js",
-      "http://a0.awsstatic.com/pricing/1/ec2/rhel-ri-light.min.js",
-      "http://a0.awsstatic.com/pricing/1/ec2/previous-generation/light_redhatlinux.min.js",
-      "http://a0.awsstatic.com/pricing/1/ec2/sles-ri-light.min.js",
-      "http://a0.awsstatic.com/pricing/1/ec2/previous-generation/light_suselinux.min.js",
-      "http://a0.awsstatic.com/pricing/1/ec2/mswin-ri-light.min.js",
-      "http://a0.awsstatic.com/pricing/1/ec2/previous-generation/light_mswin.min.js",
-      "http://a0.awsstatic.com/pricing/1/ec2/mswinSQL-ri-light.min.js",
-      "http://a0.awsstatic.com/pricing/1/ec2/previous-generation/light_mswinsqlstd.min.js",
-      "http://a0.awsstatic.com/pricing/1/ec2/mswinSQLWeb-ri-light.min.js",
-      "http://a0.awsstatic.com/pricing/1/ec2/previous-generation/light_mswinsqlweb.min.js"
-    ],
-    "mediumReservation": [
-      "http://a0.awsstatic.com/pricing/1/ec2/linux-ri-medium.min.js",
-      "http://a0.awsstatic.com/pricing/1/ec2/previous-generation/medium_linux.min.js",
-      "http://a0.awsstatic.com/pricing/1/ec2/rhel-ri-medium.min.js",
-      "http://a0.awsstatic.com/pricing/1/ec2/previous-generation/medium_redhatlinux.min.js",
-      "http://a0.awsstatic.com/pricing/1/ec2/sles-ri-medium.min.js",
-      "http://a0.awsstatic.com/pricing/1/ec2/previous-generation/medium_suselinux.min.js",
-      "http://a0.awsstatic.com/pricing/1/ec2/mswin-ri-medium.min.js",
-      "http://a0.awsstatic.com/pricing/1/ec2/previous-generation/medium_mswin.min.js",
-      "http://a0.awsstatic.com/pricing/1/ec2/mswinSQL-ri-medium.min.js",
-      "http://a0.awsstatic.com/pricing/1/ec2/previous-generation/medium_mswinsqlstd.min.js",
-      "http://a0.awsstatic.com/pricing/1/ec2/mswinSQLWeb-ri-medium.min.js",
-      "http://a0.awsstatic.com/pricing/1/ec2/previous-generation/medium_mswinsqlweb.min.js"
-    ],
-    "heavyReservation": [
-      "http://a0.awsstatic.com/pricing/1/ec2/linux-ri-heavy.min.js",
-      "http://a0.awsstatic.com/pricing/1/ec2/previous-generation/heavy_linux.min.js",
-      "http://a0.awsstatic.com/pricing/1/ec2/rhel-ri-heavy.min.js",
-      "http://a0.awsstatic.com/pricing/1/ec2/previous-generation/heavy_redhatlinux.min.js",
-      "http://a0.awsstatic.com/pricing/1/ec2/sles-ri-heavy.min.js",
-      "http://a0.awsstatic.com/pricing/1/ec2/previous-generation/heavy_suselinux.min.js",
-      "http://a0.awsstatic.com/pricing/1/ec2/mswin-ri-heavy.min.js",
-      "http://a0.awsstatic.com/pricing/1/ec2/previous-generation/heavy_mswin.min.js",
-      "http://a0.awsstatic.com/pricing/1/ec2/mswinSQL-ri-heavy.min.js",
-      "http://a0.awsstatic.com/pricing/1/ec2/previous-generation/heavy_mswinsqlstd.min.js",
-      "http://a0.awsstatic.com/pricing/1/ec2/mswinSQLWeb-ri-heavy.min.js",
-      "http://a0.awsstatic.com/pricing/1/ec2/previous-generation/heavy_mswinsqlweb.min.js"
-    ],
-    "spot": [
-      "http://spot-price.s3.amazonaws.com/spot.js"
-    ],
-    "other": [
-      "http://a0.awsstatic.com/pricing/1/ec2/pricing-data-transfer-with-regions.min.js",
-      "http://a0.awsstatic.com/pricing/1/ec2/pricing-ebs-optimized-instances.min.js",
-      "http://a0.awsstatic.com/pricing/1/ec2/pricing-ebs.min.js",
-      "http://a0.awsstatic.com/pricing/1/ec2/pricing-elastic-ips.min.js",
-      "http://a0.awsstatic.com/pricing/1/ec2/pricing-cloudwatch.min.js",
-      "http://a0.awsstatic.com/pricing/1/ec2/pricing-elb.min.js",
-      "http://a0.awsstatic.com/pricing/1/emr/pricing-emr.min.js"
-    ]
-  })
+  ec2pricing.value("pricingUrls", [
+    // on demand
+    "http://a0.awsstatic.com/pricing/1/ec2/linux-od.min.js",
+    "http://a0.awsstatic.com/pricing/1/ec2/previous-generation/linux-od.min.js",
+    "http://a0.awsstatic.com/pricing/1/ec2/mswin-od.min.js",
+    "http://a0.awsstatic.com/pricing/1/ec2/previous-generation/mswin-od.min.js",
+    "http://a0.awsstatic.com/pricing/1/ec2/rhel-od.min.js",
+    "http://a0.awsstatic.com/pricing/1/ec2/previous-generation/rhel-od.min.js",
+    "http://a0.awsstatic.com/pricing/1/ec2/sles-od.min.js",
+    "http://a0.awsstatic.com/pricing/1/ec2/previous-generation/sles-od.min.js",
+    "http://a0.awsstatic.com/pricing/1/ec2/mswinSQL-od.min.js",
+    "http://a0.awsstatic.com/pricing/1/ec2/previous-generation/mswinSQL-od.min.js",
+    "http://a0.awsstatic.com/pricing/1/ec2/mswinSQLWeb-od.min.js",
+    "http://a0.awsstatic.com/pricing/1/ec2/previous-generation/mswinSQLWeb-od.min.js",
+
+    // light reserved
+    "http://a0.awsstatic.com/pricing/1/ec2/linux-ri-light.min.js",
+    "http://a0.awsstatic.com/pricing/1/ec2/previous-generation/light_linux.min.js",
+    "http://a0.awsstatic.com/pricing/1/ec2/rhel-ri-light.min.js",
+    "http://a0.awsstatic.com/pricing/1/ec2/previous-generation/light_redhatlinux.min.js",
+    "http://a0.awsstatic.com/pricing/1/ec2/sles-ri-light.min.js",
+    "http://a0.awsstatic.com/pricing/1/ec2/previous-generation/light_suselinux.min.js",
+    "http://a0.awsstatic.com/pricing/1/ec2/mswin-ri-light.min.js",
+    "http://a0.awsstatic.com/pricing/1/ec2/previous-generation/light_mswin.min.js",
+    "http://a0.awsstatic.com/pricing/1/ec2/mswinSQL-ri-light.min.js",
+    "http://a0.awsstatic.com/pricing/1/ec2/previous-generation/light_mswinsqlstd.min.js",
+    "http://a0.awsstatic.com/pricing/1/ec2/mswinSQLWeb-ri-light.min.js",
+    "http://a0.awsstatic.com/pricing/1/ec2/previous-generation/light_mswinsqlweb.min.js",
+
+    // medium reserved
+    "http://a0.awsstatic.com/pricing/1/ec2/linux-ri-medium.min.js",
+    "http://a0.awsstatic.com/pricing/1/ec2/previous-generation/medium_linux.min.js",
+    "http://a0.awsstatic.com/pricing/1/ec2/rhel-ri-medium.min.js",
+    "http://a0.awsstatic.com/pricing/1/ec2/previous-generation/medium_redhatlinux.min.js",
+    "http://a0.awsstatic.com/pricing/1/ec2/sles-ri-medium.min.js",
+    "http://a0.awsstatic.com/pricing/1/ec2/previous-generation/medium_suselinux.min.js",
+    "http://a0.awsstatic.com/pricing/1/ec2/mswin-ri-medium.min.js",
+    "http://a0.awsstatic.com/pricing/1/ec2/previous-generation/medium_mswin.min.js",
+    "http://a0.awsstatic.com/pricing/1/ec2/mswinSQL-ri-medium.min.js",
+    "http://a0.awsstatic.com/pricing/1/ec2/previous-generation/medium_mswinsqlstd.min.js",
+    "http://a0.awsstatic.com/pricing/1/ec2/mswinSQLWeb-ri-medium.min.js",
+    "http://a0.awsstatic.com/pricing/1/ec2/previous-generation/medium_mswinsqlweb.min.js",
+
+    // heavy reserved
+    "http://a0.awsstatic.com/pricing/1/ec2/linux-ri-heavy.min.js",
+    "http://a0.awsstatic.com/pricing/1/ec2/previous-generation/heavy_linux.min.js",
+    "http://a0.awsstatic.com/pricing/1/ec2/rhel-ri-heavy.min.js",
+    "http://a0.awsstatic.com/pricing/1/ec2/previous-generation/heavy_redhatlinux.min.js",
+    "http://a0.awsstatic.com/pricing/1/ec2/sles-ri-heavy.min.js",
+    "http://a0.awsstatic.com/pricing/1/ec2/previous-generation/heavy_suselinux.min.js",
+    "http://a0.awsstatic.com/pricing/1/ec2/mswin-ri-heavy.min.js",
+    "http://a0.awsstatic.com/pricing/1/ec2/previous-generation/heavy_mswin.min.js",
+    "http://a0.awsstatic.com/pricing/1/ec2/mswinSQL-ri-heavy.min.js",
+    "http://a0.awsstatic.com/pricing/1/ec2/previous-generation/heavy_mswinsqlstd.min.js",
+    "http://a0.awsstatic.com/pricing/1/ec2/mswinSQLWeb-ri-heavy.min.js",
+    "http://a0.awsstatic.com/pricing/1/ec2/previous-generation/heavy_mswinsqlweb.min.js",
+
+    // spot
+    "http://spot-price.s3.amazonaws.com/spot.js",
+
+    // other
+    "http://a0.awsstatic.com/pricing/1/emr/pricing-emr.min.js",
+    "http://a0.awsstatic.com/pricing/1/ec2/pricing-ebs-optimized-instances.min.js",
+
+    // other (not instance based)
+    "http://a0.awsstatic.com/pricing/1/ec2/pricing-data-transfer-with-regions.min.js",
+    "http://a0.awsstatic.com/pricing/1/ec2/pricing-ebs.min.js",
+    "http://a0.awsstatic.com/pricing/1/ec2/pricing-elastic-ips.min.js",
+    "http://a0.awsstatic.com/pricing/1/ec2/pricing-cloudwatch.min.js",
+    "http://a0.awsstatic.com/pricing/1/ec2/pricing-elb.min.js"
+  ])
 
   ec2pricing.factory("jsonpLoader", function ($window, $rootScope, $q) {
     return function (url, callbackName) {
@@ -243,20 +244,29 @@
         return null
       }
     }
-    var loadData = function (url, category) {
+    var guessCategoryFromUrl = function (url) {
+      if (url.indexOf("-od") != -1) {
+        return "onDemand"
+      } else if (url.indexOf("spot") != -1) {
+        return "spot"
+      } else if (url.indexOf("-ri-light") != -1 || url.indexOf("light_") != -1) {
+        return "lightReservation"
+      } else if (url.indexOf("-ri-medium") != -1 || url.indexOf("medium_") != -1) {
+        return "mediumReservation"
+      } else if (url.indexOf("-ri-heavy") != -1 || url.indexOf("heavy_") != -1) {
+        return "heavyReservation"
+      } else {
+        return "other"
+      }
+    }
+    var loadData = function (url) {
       return cache(url, jsonpLoader, url, "callback").then(function (data) {
-        data.pricingCategory = category
+        data.pricingCategory = guessCategoryFromUrl(url)
         data.operatingSystem = guessOsFromUrl(url)
         return data
       })
     }
-    var deferreds = []
-    angular.forEach(pricingUrls, function (urls, category) {
-      urls.forEach(function (url) {
-        var deferred = loadData(url, category)
-        deferreds.push(deferred)
-      })
-    })
-    return $q.all(deferreds).then(awsDataParser)
+    var requests = pricingUrls.map(loadData)
+    return $q.all(requests).then(awsDataParser)
   })
 }())
