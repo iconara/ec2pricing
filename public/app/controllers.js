f6f7787289e1726de18dbad4c57e1c22b93970ab
Theo <theo@iconara.net>
2014-04-20 16:14:23 +0200

    Move static initialization out of the data loaded handler

--- public/app/controllers.js
+++ public/app/controllers.js
@@ -4,27 +4,28 @@
   var ec2pricing = angular.module("ec2pricing")
 
   ec2pricing.controller("ApplicationController", function ($scope, pricingData) {
-    pricingData.then(function (data) {
-      $scope.sortBy = function (what) {
-        if (what == $scope.sortField) {
-          $scope.sortAscending = !$scope.sortAscending
-        } else {
-          $scope.sortField = what
-          $scope.sortAscending = false
-        }
+    $scope.sortBy = function (what) {
+      if (what == $scope.sortField) {
+        $scope.sortAscending = !$scope.sortAscending
+      } else {
+        $scope.sortField = what
+        $scope.sortAscending = false
       }
+    }
 
+    $scope.reservationTypes = ["lightReservation", "mediumReservation", "heavyReservation"]
+    $scope.reservationTerms = ["yrTerm1", "yrTerm3"]
+    $scope.periods = ["hourly", "daily", "weekly", "monthly", "yearly"]
+    $scope.selectedRegion = "us-east-1"
+    $scope.selectedOperatingSystem = "linux"
+    $scope.selectedReservationType = "heavyReservation"
+    $scope.selectedReservationTerm = "yrTerm1"
+    $scope.selectedPeriod = $scope.periods[0]
+
+    pricingData.then(function (data) {
       $scope.regions = data.regions
       $scope.operatingSystems = data.operatingSystems
       $scope.instanceTypes = data.instanceTypes
-      $scope.reservationTypes = ["lightReservation", "mediumReservation", "heavyReservation"]
-      $scope.reservationTerms = ["yrTerm1", "yrTerm3"]
-      $scope.periods = ["hourly", "daily", "weekly", "monthly", "yearly"]
-      $scope.selectedRegion = "us-east-1"
-      $scope.selectedOperatingSystem = "linux"
-      $scope.selectedReservationType = "heavyReservation"
-      $scope.selectedReservationTerm = "yrTerm1"
-      $scope.selectedPeriod = $scope.periods[0]
 
       $scope.sortBy("apiName")
     })
