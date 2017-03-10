43fec7a04ab66cecff07496d52a6813e220e8bc4
Theo <theo@iconara.net>
2017-03-10 20:32:36 +0100

    Change the main view to query for prices
    And sort the instances by size, not lexicographically

--- public/app/main.js
+++ public/app/main.js
@@ -14,6 +14,7 @@ const app = new Vue({
             <th>RAM</th>
             <th>Disk</th>
             <th>Network performance</th>
+            <th>Hourly rate</th>
           </tr>
         </thead>
         <tbody>
@@ -23,6 +24,7 @@ const app = new Vue({
             <td>{{instanceType.memory}}</td>
             <td>{{instanceType.storage}}</td>
             <td>{{instanceType.networkPerformance}}</td>
+            <td>{{instanceType.hourlyRate}}</td>
           </tr>
         </tbody>
       </table>
@@ -47,18 +49,71 @@ const app = new Vue({
       })
       loader.loadDatabase().then((db) => {
         this.loaded = true
-        db.each("SELECT * FROM instance_type", null, (row) => {
-          if (row.instance_type.length > 0) {
-            this.instanceTypes.push({
-              name: row.instance_type,
-              vcpus: row.vcpus,
-              memory: row.memory,
-              storage: row.storage,
-              networkPerformance: row.network_performance
-            })
-          }
+        let sql = `
+          SELECT
+            instance_type,
+            vcpus,
+            memory,
+            storage,
+            network_performance,
+            hourly_rate
+          FROM instance_type it
+          LEFT JOIN cost c ON (it.instance_type_id = c.instance_type_id)
+          NATURAL JOIN purchase_option
+          NATURAL JOIN lease_contract_length
+          NATURAL JOIN offering_class
+          NATURAL JOIN location
+          NATURAL JOIN operating_system
+          NATURAL JOIN tenancy
+          NATURAL JOIN license_model
+          NATURAL JOIN preinstalled_software
+          WHERE purchase_option_id = 0
+          AND lease_contract_length_id = 0
+          AND offering_class_id = 0
+          AND location_id = 9
+          AND operating_system_id = 1
+          AND tenancy_id = 3
+          AND license_model_id = 4
+          AND preinstalled_software_id = 1
+        `
+        db.each(sql, null, (row) => {
+          this.instanceTypes.push({
+            name: row.instance_type,
+            vcpus: row.vcpus,
+            memory: row.memory,
+            storage: row.storage,
+            networkPerformance: row.network_performance,
+            hourlyRate: row.hourly_rate
+          })
+          this.instanceTypes.sort((it1, it2) => this.instanceTypeSort(it1, it2))
         })
       })
+    },
+
+    instanceTypeSort(it1, it2) {
+      let [family1, size1] = it1.name.split(".")
+      let [family2, size2] = it2.name.split(".")
+      let familyResult = family1.localeCompare(family2)
+      if (familyResult == 0) {
+        return this.sizeToNumber(size1) - this.sizeToNumber(size2)
+      } else {
+        return familyResult
+      }
+    },
+
+    sizeToNumber(size) {
+      let sizes = ["nano", "micro", "small", "medium", "large", "xlarge"]
+      let n = sizes.indexOf(size)
+      if (n > -1) {
+        return n
+      } else {
+        let matches = size.match(/^(\d+)xlarge/)
+        if (matches) {
+          return sizes.length + parseInt(matches[1])
+        } else {
+          return -1
+        }
+      }
     }
   }
 })
