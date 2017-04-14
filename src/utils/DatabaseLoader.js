3dfd42d3adb149b68335fb21374117b7a1a8bf88
Theo <theo@iconara.net>
2017-04-14 18:27:43 +0200

    Refactor the database loading XHR code
    I can't believe how hopeless the XHR API is.

--- src/utils/DatabaseLoader.js
+++ src/utils/DatabaseLoader.js
@@ -19,26 +19,32 @@ export default class DatabaseLoader {
         let xhr = new XMLHttpRequest()
         xhr.open('GET', this.location, true)
         xhr.responseType = 'arraybuffer'
-        xhr.addEventListener('progress', (event) => {
-          let uncompressedLength = xhr.getResponseHeader('x-amz-meta-x-amz-meta-uncompressed-content-length')
-          if (event.lengthComputable) {
-            for (let listener of this.progressListeners) {
-              listener(event.loaded, event.total)
-            }
-          } else if (uncompressedLength) {
-            listener(event.loaded, parseInt(uncompressedLength))
-          }
-        })
-        xhr.addEventListener('error', (event) => {
-          reject(event)
-        })
-        xhr.addEventListener('load', () => {
-          this.progressListeners = null
-          this.database = new window.SQL.Database(new Uint8Array(xhr.response))
-          resolve(this.database)
-        })
+        xhr.addEventListener('progress', event => this._progress(event))
+        xhr.addEventListener('error', reject)
+        xhr.addEventListener('load', resolve)
         xhr.send()
-      })
+      }).then((event) => this._finalize(event))
     }
+  }
+
+  _progress (event) {
+    let uncompressedLength = event.target.getResponseHeader('x-amz-meta-x-amz-meta-uncompressed-content-length')
+    let totalSize = null
+    if (event.lengthComputable) {
+      totalSize = event.total
+    } else if (uncompressedLength) {
+      totalSize = parseInt(uncompressedLength)
+    }
+    if (totalSize) {
+      for (let listener of this.progressListeners) {
+        listener(event.loaded, totalSize)
+      }
+    }
+  }
+
+  _finalize (event) {
+    this.progressListeners = null
+    this.database = new window.SQL.Database(new Uint8Array(event.target.response))
+    return this.database
   }
 }
